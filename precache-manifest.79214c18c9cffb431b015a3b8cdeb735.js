67e1c6104d2114d6bfa137caf453f22cff731f72
tylersanderson <tylersanderson@hotmail.com>
2019-08-10 07:08:22 -0500

    Updates

---
+++ precache-manifest.79214c18c9cffb431b015a3b8cdeb735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18257de2e0c63698a83b483f4b264642",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "200c6d7eecdb7727964e",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "42a6e6ed6573fe777dd8",
+    "url": "/robofriends/static/css/main.3ffab221.chunk.css"
+  },
+  {
+    "revision": "200c6d7eecdb7727964e",
+    "url": "/robofriends/static/js/2.02ef720e.chunk.js"
+  },
+  {
+    "revision": "42a6e6ed6573fe777dd8",
+    "url": "/robofriends/static/js/main.8d622fc9.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
